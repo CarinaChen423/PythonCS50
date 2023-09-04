b578f21a59058ca38abfc3a2b1530d054186204f
CarinaChen423 <chaselc011@gmail.com>
2023-09-04 18:30:05 +0200

    Add project files.

---
+++ PythonApplication1/Python1.py
@@ -0,0 +1,10 @@
+
+print("Welcome to my world")
+
+name=input("What's your name? ")
+#capitalize user's name
+name=name.capitalize()
+#remove withespace from str
+name=name.strip()
+print("Hello, " + name)
+
