22cc549b6cc9bc12226ea2d01a6ba34bbad3fa15
CarinaChen423 <chaselc011@gmail.com>
2023-09-12 21:26:38 +0200

    9/12/2023

--- PythonApplication1/Python1.py
+++ PythonApplication1/Python1.py
@@ -6,5 +6,23 @@ name=input("What's your name? ")
 name=name.capitalize()
 #remove withespace from str
 name=name.strip()
-print("Hello, " + name)
+#new line
+print("Hello,\n" + name, )
 
+#end
+print("Hello,", end="??")
+print(name)
+
+#with one space
+print("Hello,",name)
+
+#spe, no space
+print("Hello,",name,sep="???")
+
+#print quotes
+print('Hello,"550w"')
+print("Hello,'Sophon'")
+print("Hello,\"R\"")
+
+#f string
+print(f"Hi,{name}")
